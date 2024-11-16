d396407c7fc71959981a564c1760c2e48ee0845f
Martin Penkava <martin.pinky@seznam.cz>
2024-11-16 12:35:03 +0100

    Initial commit

---
+++ src/main/java/com/springapi/shopsample/ShopsampleApplication.java
@@ -0,0 +1,13 @@
+package com.springapi.shopsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapi/shopsample/ShopsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapi.shopsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
